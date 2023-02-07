1316a77c72763a21be35b4993c72a5c2e9139e45
LPTruong <phitruongvp247@gmail.com>
2023-02-07 17:01:49 +0700

    update datasource layer

--- lib/features/data/datasources/warning_local_data_source.dart
+++ lib/features/data/datasources/warning_local_data_source.dart
@@ -1,23 +1,45 @@
-import 'package:dartz/dartz.dart';
-import 'package:project_alpha/features/data/models/warning_model.dart';
+import 'dart:convert';
 
-abstract class WarningLocalDataSource{
+import 'package:dartz/dartz.dart';
+import 'package:project_alpha/core/error/exception.dart';
+import 'package:project_alpha/features/data/models/warning_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class WarningLocalDataSource {
   Future<List<WarningModel>> getCachedWarnings();
   Future<Unit> cacheWarnings(List<WarningModel> warningModels);
-
 }
 
-class WarningLocalDataSourceImpl implements WarningLocalDataSource{
+const CACHED_WARNINGS  = "CACHED_WARNINGS";
+
+
+class WarningLocalDataSourceImpl implements WarningLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  WarningLocalDataSourceImpl({required this.sharedPreferences});
+
   @override
   Future<Unit> cacheWarnings(List<WarningModel> warningModels) {
-    // TODO: implement cacheWarnings
-    throw UnimplementedError();
+    List warningModelsToJson = warningModels
+        .map<Map<String, dynamic>>((warningModels) => warningModels.toJson())
+        .toList();
+    sharedPreferences.setString(
+        CACHED_WARNINGS, json.encode(warningModelsToJson));
+    return Future.value(unit);
   }
 
   @override
   Future<List<WarningModel>> getCachedWarnings() {
-    // TODO: implement getCachedWarnings
-    throw UnimplementedError();
+    final jsonString = sharedPreferences.getString(CACHED_WARNINGS);
+    if (jsonString != null) {
+      List decodeJsonData = json.decode(jsonString);
+      List<WarningModel> jsonToWarningModels = decodeJsonData
+          .map<WarningModel>(
+              (jsonWarningModel) => WarningModel.fromJson(jsonWarningModel))
+          .toList();
+        return Future.value(jsonToWarningModels);
+    } else {
+      throw EmptyCacheException();
+    }
   }
-
 }
--- lib/features/data/datasources/warning_remote_data_source.dart
+++ lib/features/data/datasources/warning_remote_data_source.dart
@@ -1,39 +1,97 @@
-import 'package:dartz/dartz.dart';
-import 'package:project_alpha/features/data/models/warning_model.dart';
+import 'dart:convert';
+import 'dart:html';
 
-abstract class WarningRemoteDataSource{
+import 'package:dartz/dartz.dart';
+import 'package:project_alpha/core/error/exception.dart';
+import 'package:project_alpha/features/data/models/warning_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class WarningRemoteDataSource {
   Future<List<WarningModel>> getAllWarnings();
   Future<Unit> deleteWarning(String warningId);
   Future<Unit> updateWarning(WarningModel warningModel);
   Future<Unit> addWarning(WarningModel warningModel);
-  
 }
 
-class WarningRemoteDataSourceImpl implements WarningRemoteDataSource{
+const BASE_URL = "https://63e0b7f359bb472a74275867.mockapi.io";
+
+class WarningRemoteDataSourceImpl implements WarningRemoteDataSource {
+  final http.Client client;
+
+  WarningRemoteDataSourceImpl({required this.client});
+
   @override
-  Future<Unit> addWarning(WarningModel warningModel) {
-    // TODO: implement addWarning
-    throw UnimplementedError();
+  Future<List<WarningModel>> getAllWarnings() async {
+    final response = await client.get(Uri.parse(BASE_URL + "/warnings/"));
+
+    if (response.statusCode == 200) {
+      final List decodeJson = json.decode(response.body) as List;
+      final List<WarningModel> warningModels = decodeJson
+          .map<WarningModel>(
+              (jsonWarningModel) => WarningModel.fromJson(jsonWarningModel))
+          .toList();
+
+      return warningModels;
+    } else {
+      throw ServerException();
+    }
   }
 
   @override
-  Future<Unit> deleteWarning(String warningId) {
-    // TODO: implement deleteWarning
-    throw UnimplementedError();
+  Future<Unit> addWarning(WarningModel warningModel) async {
+    final body = {
+      'id': warningModel.id,
+      'title': warningModel.title,
+      'content': warningModel.content,
+      'level': warningModel.level,
+      'category': warningModel.category,
+      'image': warningModel.image,
+      'createAt': warningModel.createAt,
+      'author': warningModel.author,
+    };
+    final response =
+        await client.post(Uri.parse(BASE_URL + "/warnings/"), body: body);
+    if (response.statusCode == 201) {
+      return Future.value(unit);
+    } else {
+      throw ServerException();
+    }
   }
 
   @override
-  Future<List<WarningModel>> getAllWarnings() {
-    // TODO: implement getAllWarnings
-    throw UnimplementedError();
+  Future<Unit> deleteWarning(String warningId) async {
+    final response =
+        await client.delete(Uri.parse(BASE_URL + "/warnings/$warningId"));
+    if (response.statusCode == 200) {
+      return Future.value(unit);
+    } else {
+      throw ServerException();
+    }
   }
 
   @override
-  Future<Unit> updateWarning(WarningModel warningModel) {
-    // TODO: implement updateWarning
-    throw UnimplementedError();
+  Future<Unit> updateWarning(WarningModel warningModel) async {
+    final warningId = warningModel.id;
+    final body = {
+      'id': warningModel.id,
+      'title': warningModel.title,
+      'content': warningModel.content,
+      'level': warningModel.level,
+      'category': warningModel.category,
+      'image': warningModel.image,
+      'createAt': warningModel.createAt,
+      'author': warningModel.author,
+    };
+
+    final response = await client.patch(
+      Uri.parse(BASE_URL + "/warnings/$warningId"),
+      body: body,
+    );
+    
+    if (response.statusCode == 200) {
+      return Future.value(unit);
+    } else {
+      throw ServerException();
+    }
   }
-  
 }
-
- 
